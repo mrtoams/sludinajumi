7eb4512e2e4af856ef67f959635c45313b28f642
Toms Biluns <toms.biluns@gmail.com>
2019-09-03 22:15:40 +0300

    Navbar, database, postController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Sludinājumi</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <title>Sludinājumi</title>
 
 
     </head>
     <body>
-       @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
+       
     </body>
 </html>
